39952f51273e7e5d37bd557bd50abdf91776a154
Matt Travi <programmer@travi.org>
2008-11-21 05:30:02 +0000

    undoing load content object by directory unordered list is still being redeclared

--- php/framework/travi.framework.php
+++ php/framework/travi.framework.php
@@ -20,14 +20,14 @@
 
  //Content Objects
  require_once('objects/content/contentObject.class.php');
- importObjects('objects/content/'); //need to eliminate the duplicate unordered list from elements.class.php
- /*require_once('objects/content/form.class.php');
+ //importObjects('objects/content/'); //need to eliminate the duplicate unordered list from elements.class.php
+ require_once('objects/content/form.class.php');
  require_once('objects/content/entityList.class.php');
  require_once('objects/content/feedcreator.class.php');
  require_once('objects/content/gallery.class.php');
  require_once('objects/content/list.class.php');
  require_once('objects/content/previewWindow.class.php');
- require_once('objects/content/accordion.class.php');*/
+ require_once('objects/content/accordion.class.php');
 
  //Ajax Content Objects
  require_once('objects/content/ajax/fileUpload.ajax.class.php');
