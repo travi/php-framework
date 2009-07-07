59d9e77c86643ef02e26003a47a96b65f9b61a51
Matt Travi <programmer@travi.org>
2009-07-07 01:36:44 +0000

    gallery updates

--- php/framework/objects/content/gallery.class.php
+++ php/framework/objects/content/gallery.class.php
@@ -3,10 +3,12 @@
 class Gallery extends ContentObject
 {
 	var $thumbs = array();
+	var $albums = array();
 	var $thumbs_theme;
 
-    function Gallery()
+    function Gallery($albums)
     {
+    	$this->albums = $albums['albums'];
 		$this->setDependencies();
     }
     function setThumbs($thumbs=array())
@@ -44,18 +46,21 @@ class Gallery extends ContentObject
 		$this->addStyleSheet("/resources/shared/js/jquery/plugins/jcarousel/skins/tango/skin.css");
 
     }
-	function menu()
+	function menu($albums=array())
 	{
-		return '
-			<div id="msc_menu">
-			<!--
-				<ul>
-						<li>&nbsp;|&nbsp;<a href="#">camaro</a></li>
-						<li>&nbsp;|&nbsp;<a href="#">corvette</a></li>
-						<li>&nbsp;|&nbsp;<a href="#">truck</a></li>
-				</ul>
-			-->
-			</div>';
+		$content = '
+			<ul id="albumMenu">';
+				
+		foreach($albums as $album)
+		{
+			$content .= '
+					<li>&nbsp;|&nbsp;<a href="/gallery/?album='.$album['name'].'">'.$album['name'].'</a></li>';
+		}
+						
+		$content .= '
+			</ul>';
+			
+		return $content;
 	}
     function jcarousel()
     {
@@ -75,27 +80,7 @@ class Gallery extends ContentObject
     }
     function __toString()
     {
-    	return $this->menu().$this->jcarousel().'
-
-		<!--	<div id="msc_image">
-
-				<div id="image_div">
-					<div id="image_container"> -->
-					<!--
-						<div id="image_nav">
-							<div class="button left"><a href="javascript:;" onclick="prevImage()">&#171; previous image</a></div>
-							<div class="button right"><a href="javascript:;" onclick="nextImage()">next image &#187;</a></div>
-						</div>
-					-->
-				<!--		<img id="preview_pos" class="imagen preview reflect" src="images/camaro_1.jpg" title="image1" alt="image1" />
-						<div id="image_title"></div>
-					</div>
-				</div>
-				<img src="images/camaro_1.jpg" alt="preview" class="preview" id="preview_overlay"/>
-				<div id="gallery_info">
-					click on a gallery from the menu above
-				</div>
-			</div> -->';
+    	return $this->menu($this->albums).$this->jcarousel();
     }
 }
 ?>
