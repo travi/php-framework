1264af5041ec841d54de75bf276abc40101c1e45
unknown <devnull@localhost>
2009-11-14 19:38:06 +0000

    Unit testing frameworks for php and js

---
+++ php/framework/tests/helloWorld.test.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Created on Nov. 11, 2009
+ * By Matt Travi
+ * programmer@travi.org
+ */require_once(dirname(__FILE__) . '/../../simpletest/autorun.php');
+
+class HelloWorld extends UnitTestCase {
+	function testHello(){
+		$this->assertTrue(true);
+	}
+}
+?>
