e350f6597346b4aca0e51d158ed58397534f9a14
Matt Travi <programmer@travi.org>
2010-11-27 15:25:16 -0600

    gallery admin list page templated

--- php/framework/objects/content/navigation/accordion.class.php
+++ php/framework/objects/content/navigation/accordion.class.php
@@ -14,31 +14,6 @@ class Accordion extends NavigationObject
 		$this->addJavaScript('jqueryUi');
 		$this->addJsInit("$('.accordion').accordion({animated: 'easeslide',header: 'dt'});");	
 	}
-	
-	public function __toString()
-	{
-		$content = '
-		<dl class="accordion contentNav">';
-
-		foreach($this->sections as $title => $section)
-		{
-			$content .= '
-			<dt>'.$title.'</dt>
-				<dd>
-					';
-
-				$sectionContent = $section->getContent();
-			$content .= (!empty($sectionContent))?$section:'&nbsp;';
-
-			$content .= '
-				</dd>';
-		}
-
-		$content .= '
-		</dl>';
-
-		return $content;
-	}
 }
  
 ?>
