34e4d47cd43be38ab8114223ee766f867f900754
Matt Travi <programmer@travi.org>
2011-01-09 23:00:37 -0600

    loading site custom font list

--- php/framework/src/http/Response.class.php
+++ php/framework/src/http/Response.class.php
@@ -17,12 +17,25 @@ class Response extends xhtmlPage
         $this->setSiteHeader($config['siteHeader']);
         $this->setTheme('/resources/css/' . $config['theme']['site']);
 
+        if(!empty($config['customFonts']))
+        {
+            $this->loadCustomFonts($config);
+        }
+
         //temporarily set the layout template here until moving it to $View
         $this->layoutTemplate = $config['template']['layout'];
 
         //temporarily set smartyConfig to work around the fact that xhtml.class is currently being used
         $this->smartyConfig = $config['smarty'];
         
+    }
+
+    private function loadCustomFonts($config)
+    {
+        foreach($config['customFonts'] as $font)
+        {
+            $this->addStyleSheet($font);
+        }
     }
 
     public function respond()
