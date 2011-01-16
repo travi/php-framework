9a23e13249e1dfd779a0f3840bbf9160fa465bfd
Matt Travi <programmer@travi.org>
2011-01-16 10:07:06 -0600

    ran jsInit through JSLint

--- php/framework/objects/content/navigation/tabs.class.php
+++ php/framework/objects/content/navigation/tabs.class.php
@@ -10,7 +10,7 @@ class Tabs extends NavigationObject
 	function Tabs($options=array())
 	{
 		$this->addJavaScript('jqueryUi');
-		$this->addJsInit("$('.ui-tabs').tabs({selected:0,fx:{opacity:'toggle', height:'toggle'}});");
+		$this->addJsInit("$('.ui-tabs').tabs({selected: 0, fx: {opacity: 'toggle', height: 'toggle'}});");
         $this->setTemplate('components/tabs.tpl');
 	}
 }
