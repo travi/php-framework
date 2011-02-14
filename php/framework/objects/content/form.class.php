f89ecdbf9b2aadcde5f9af1688bf294fae0c1cee
Matt Travi <programmer@travi.org>
2011-02-13 19:43:07 -0600

    SelectionBox tests

--- php/framework/objects/content/form.class.php
+++ php/framework/objects/content/form.class.php
@@ -17,6 +17,8 @@ require_once(dirname(__FILE__).'/../../src/components/form/RichTextArea.php');
 require_once(dirname(__FILE__).'/../../src/components/form/SubmitButton.php');
 require_once(dirname(__FILE__).'/../../src/components/form/DateInput.php');
 require_once(dirname(__FILE__).'/../../src/components/form/NoteArea.php');
+require_once(dirname(__FILE__).'/../../src/components/form/Choices.php');
+require_once(dirname(__FILE__).'/../../src/components/form/SelectionBox.php');
 
 //////////////////////////////////////////////////////////////////////
 //						 		Form								//
@@ -179,44 +181,6 @@ class Button
 //					Fields Giving Numerous Choices					//
 //////////////////////////////////////////////////////////////////////
 
-class SelectionBox extends Choices
-{
-	private $optGroups = array();
-	
-	public function __construct($options=array())
-	{
-		$this->addOption("Select One");
-		parent::__construct($options);
-        $this->setTemplate('components/form/selectionBox.tpl');
-	}
-	protected function optionAdder($options=array())
-	{
-		if($this->settings['optGroups'])
-		{
-			foreach($options as $optGroup => $values)
-			{				
-				$this->optGroups[$optGroup] = array();
-				foreach($values as $value)
-				{
-					$selected=false;
-					$disabled=false;
-				
-					$optionAR = array(	option		=> $value['label'],
-										value		=> $value['value'],
-										selected	=> $selected,
-										disabled	=> $disabled);
-										
-					array_push($this->optGroups[$optGroup],$optionAR);					
-				}
-			}
-		}
-		else
-		{
-			parent::optionAdder($options);
-		}
-		
-	}
-}
 class RadioButtons extends Choices
 {
 	public function __construct($options=array())
---
+++ php/framework/src/components/form/SelectionBox.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created on Feb 13, 2011
+ * By Matt Travi
+ * programmer@travi.org
+ */
+require_once('Choices.php');
+
+class SelectionBox extends Choices
+{
+	private $optGroups = array();
+
+	public function __construct($options=array())
+	{
+		$this->addOption("Select One");
+		parent::__construct($options);
+        $this->setTemplate('components/form/selectionBox.tpl');
+	}
+	protected function optionAdder($options=array())
+	{
+		if($this->settings['optGroups'])
+		{
+			foreach($options as $optGroup => $values)
+			{
+				$this->optGroups[$optGroup] = array();
+				foreach($values as $value)
+				{
+					$selected=false;
+					$disabled=false;
+
+					$optionAR = array(	option		=> $value['label'],
+										value		=> $value['value'],
+										selected	=> $selected,
+										disabled	=> $disabled);
+
+					array_push($this->optGroups[$optGroup],$optionAR);
+				}
+			}
+		}
+		else
+		{
+			parent::optionAdder($options);
+		}
+
+	}
+}
+ 
