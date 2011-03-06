6e54300105cc9934a3f0ba8a9b50705fe7cf775e
Matt Travi <programmer@travi.org>
2011-03-06 12:30:12 -0600

    changed from tab to space indentations

--- php/framework/src/components/form/Field.php
+++ php/framework/src/components/form/Field.php
@@ -4,7 +4,7 @@ require_once(dirname(__FILE__).'/../../../objects/content/contentObject.class.ph
 
 interface Field
 {
-	public function getValidations();
-	public function getName();
-	public function addValidation($validation);
+    public function getValidations();
+    public function getName();
+    public function addValidation($validation);
 }
