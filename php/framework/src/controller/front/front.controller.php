f23bafd15f88e4ac490313ffb67500726b6458a7
Matt Travi <programmer@travi.org>
2011-03-06 19:02:51 -0600

    checkstyle updates

--- php/framework/src/controller/front/front.controller.php
+++ php/framework/src/controller/front/front.controller.php
@@ -87,10 +87,11 @@ class FrontController
                     . $this->Request->getAction()
                     . '.tpl';
         
-        if (empty($template)
-                && file_exists($this->config['sitePath']
-                        . '/app/view/pages' . $templateByConvention)
-        ) {
+        if (
+            empty($template)
+            && file_exists($this->config['sitePath'] . '/app/view/pages' . $templateByConvention)
+        )
+        {
             $this->Response->setPageTemplate($templateByConvention);
         }
         //TODO: this should be moved out to the head template once the other sites support mobile
