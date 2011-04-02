6dccc5c8910656c50e30dd8856d74d72737c6eaa
Matt Travi <programmer@travi.org>
2011-04-02 09:42:29 -0500

    fixed bug preventing button value from being set

--- php/framework/src/components/form/SubmitButton.php
+++ php/framework/src/components/form/SubmitButton.php
@@ -10,37 +10,39 @@ require_once('inputs/Input.php');
 
 class SubmitButton extends Input
 {
-	protected $confirmation;
+    protected $confirmation;
 
-	public function __construct($options)
-	{
-		parent::__construct($options);
+    public function __construct($options)
+    {
+        parent::__construct($options);
         $this->label = "";
-		$this->setType("submit");
-		$this->setName("Submit");
-		if(!empty($options['class']))
-			$this->setClass($options['class']);
-		else
-			$this->setClass("submitButton");
-		$this->value = $options['label'];
+        $this->setType("submit");
+        $this->setName("Submit");
+        if(!empty($options['class']))
+            $this->setClass($options['class']);
+        else
+            $this->setClass("submitButton");
+        $this->setValue($options['label']);
         $this->setTemplate('components/form/input.tpl');
         $this->addJavaScript('jqueryUi');
         $this->addJsInit('$("input[type=submit]").button()');
-	}
-	//TODO need to replace this technique using UI dialog
-	public function setConfirmation($confirmation)
-	{
-		$this->confirmation = $confirmation;
-	}
-//	public function __toString()
-//	{
-//		$string = '
-//						<input type="'.$this->type.'" name="'.$this->name.'" id="'.$this->name.'" value="'.$this->value.
-//					'" class="'.$this->class.'"';
-//		if(!empty($this->confirmation))
-//			$string .= ' onclick="if (confirm(\''.$this->confirmation.'\')) return true; else return false;"';
-//		$string .= '/>';
+    }
+
+    //TODO need to replace this technique using UI dialog
+    public function setConfirmation($confirmation)
+    {
+        $this->confirmation = $confirmation;
+    }
+
+//    public function __toString()
+//    {
+//        $string = '
+//                        <input type="'.$this->type.'" name="'.$this->name.'" id="'.$this->name.'" value="'.$this->value.
+//                    '" class="'.$this->class.'"';
+//        if(!empty($this->confirmation))
+//            $string .= ' onclick="if (confirm(\''.$this->confirmation.'\')) return true; else return false;"';
+//        $string .= '/>';
 //
-//		return $string;
-//	}
+//        return $string;
+//    }
 }
--- php/framework/src/components/form/inputs/Input.php
+++ php/framework/src/components/form/inputs/Input.php
@@ -55,6 +55,9 @@ abstract class Input extends ContentObject implements Field
     {
         return $this->type;
     }
+    public function setValue($value){
+        $this->value = $value;
+    }
     public function getValue()
     {
         return $this->value;
