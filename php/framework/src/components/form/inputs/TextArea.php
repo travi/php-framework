1c9b8a94580f0ec7a1c3902a67ea72682bf240d5
Matt Travi <programmer@travi.org>
2011-04-03 00:39:00 -0500

    checkstyle fixes

--- php/framework/src/components/form/inputs/TextArea.php
+++ php/framework/src/components/form/inputs/TextArea.php
@@ -10,16 +10,17 @@ require_once('Input.php');
 
 class TextArea extends Input
 {
-	private $rows;
+    private $rows;
 
-	public function __construct($options)
-	{
-		parent::__construct($options);
-		$this->setClass("textInput");
-		$this->rows = $options['rows'];
+    public function __construct($options)
+    {
+        parent::__construct($options);
+        $this->setClass("textInput");
+        $this->rows = $options['rows'];
         $this->setTemplate('components/form/textArea.tpl');
-	}
-	public function getRows()
+    }
+    
+    public function getRows()
     {
         return $this->rows;
     }
