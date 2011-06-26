f5b2da3a99198298bbbceba45d7c11e1535d4aa8
Matt Travi <programmer@travi.org>
2011-06-26 12:15:43 -0500

    checkstyle fixes

--- php/framework/controllers/displayProcess.php
+++ php/framework/controllers/displayProcess.php
@@ -2,7 +2,7 @@
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     content();
-} elseif($_SERVER['REQUEST_METHOD'] === 'POST') {
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
     list($status, $msg, $redirectTo) = process();
     $page->redirect($status, $msg, $redirectTo);
 }
