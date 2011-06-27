c3cbba20dcce7d233dbc8d515dd8986a1b24f1b1
Matt Travi <programmer@travi.org>
2011-06-26 22:07:25 -0500

    checkstyle fixes

--- php/framework/objects/content/previewWindow.class.php
+++ php/framework/objects/content/previewWindow.class.php
@@ -3,41 +3,41 @@ require_once('contentObject.class.php');
 
 class PreviewWindow extends ContentObject
 {
-	var $url;
-	var $text;
-	var $field;
+    var $url;
+    var $text;
+    var $field;
 
     function PreviewWindow()
     {
-    	$this->addJavaScript('/reusable/js/windows_js/javascripts/prototype.js');
-    	$this->addJavaScript('/reusable/js/windows_js/javascripts/window.js');
-    	$this->addJavaScript('/reusable/js/windows_js/previewURL.js');
+        $this->addJavaScript('/reusable/js/windows_js/javascripts/prototype.js');
+        $this->addJavaScript('/reusable/js/windows_js/javascripts/window.js');
+        $this->addJavaScript('/reusable/js/windows_js/previewURL.js');
 
-    	$this->addStyleSheet('/reusable/js/windows_js/themes/default.css');
-    	$this->addStyleSheet('/reusable/js/windows_js/themes/alphacube.css');
+        $this->addStyleSheet('/reusable/js/windows_js/themes/default.css');
+        $this->addStyleSheet('/reusable/js/windows_js/themes/alphacube.css');
     }
     function setURL($url)
     {
-    	$this->url = $url;
+        $this->url = $url;
     }
     function setLinkText($text)
     {
-    	$this->text = $text;
+        $this->text = $text;
     }
     function setField($field)
     {
-    	$this->field = $field;
+        $this->field = $field;
     }
-	function __toString()
-	{
-		$link = '<a href="#" onclick="';
-		if(isset($this->field))
-			$link .= "previewUrlFromTextField('".$this->field."')";
-		else if (isset($this->url))
-			$link .= "previewURL('".$this->url."')";
-		$link .= '">'.$this->text.'</a>';
+    function __toString()
+    {
+        $link = '<a href="#" onclick="';
+        if(isset($this->field))
+            $link .= "previewUrlFromTextField('".$this->field."')";
+        else if (isset($this->url))
+            $link .= "previewURL('".$this->url."')";
+        $link .= '">'.$this->text.'</a>';
 
-		return $link;
-	}
+        return $link;
+    }
 }
 ?>
--- php/framework/objects/utility/dataFormat.class.php
+++ php/framework/objects/utility/dataFormat.class.php
@@ -1,31 +1,26 @@
 <?php
-/*
- * Created on Sept 12, 2010
- * By Matt Travi
- */
 
 abstract class dataFormatter
 {
-	protected $data = array();
-	
-	public function setData($data = array())
-	{
-		$this->data = $data;
-	}
-	public function format()
-	{
-		return print_r($this->data);
-	}
+    protected $data = array();
+
+    public function setData($data = array())
+    {
+        $this->data = $data;
+    }
+    public function format()
+    {
+        return print_r($this->data);
+    }
 }
 
 class jsonFormatter extends dataFormatter
 {
-	public function format()
-	{
-    	$output = json_encode($this->data);
-		header('Content-Type: application/json');
-		
-		return $output;
-	}
+    public function format()
+    {
+        $output = json_encode($this->data);
+        header('Content-Type: application/json');
+
+        return $output;
+    }
 }
-?>
