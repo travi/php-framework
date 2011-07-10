cf826330bded3834f90c68cd2b078b17a5c069c4
Matt Travi <programmer@travi.org>
2011-07-10 00:59:26 -0500

    attempt to fix path issue

--- php/framework/test/php/controller/AbstractControllerTest.php
+++ php/framework/test/php/controller/AbstractControllerTest.php
@@ -1,9 +1,9 @@
 <?php
 require_once 'PHPUnit/Framework.php';
 
-require_once '/Users/travi/development/include/php/framework/src/controller/abstract.controller.php';
-require_once '/home/travi/include/php/framework/src/http/Request.class.php';
-require_once '/home/travi/include/php/framework/src/exception/NotFound.exception.php';
+require_once dirname(__FILE__).'/../../../src/controller/abstract.controller.php';
+require_once dirname(__FILE__).'/../../../src/http/Request.class.php';
+require_once dirname(__FILE__).'/../../../src/exception/NotFound.exception.php';
 
 /**
  * Test class for AbstractController.
