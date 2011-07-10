ec8fe1314187ac4aca29fe74c6fb50d8d0a0a12c
Matt Travi <programmer@travi.org>
2011-07-10 09:37:00 -0500

    fix to next path issue

--- php/framework/test/php/content/DependantObjectTest.php
+++ php/framework/test/php/content/DependantObjectTest.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'PHPUnit/Framework.php';
 
-require_once '/Users/travi/development/include/php/framework/objects/dependantObject.class.php';
+require_once dirname(__FILE__).'/../../../objects/dependantObject.class.php';
 
 /**
  * Test class for DependantObject.
