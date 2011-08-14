a5fab1527092f29190be50fc32612cee1dee544d
Matt Travi <programmer@travi.org>
2011-08-14 10:27:42 -0500

    switched qunit tests using 'same' to 'strictEqual'

---
+++ php/framework/test/js/qunit.stub.js
@@ -0,0 +1,11 @@
+module = function (name, options) {};
+
+test = function (name, test) {};
+
+strictEqual = function (expected, actual, message) {};
+ok = function (boolean, message) {};
+
+start = function () {};
+stop = function () {};
+
+expect = function (numberOfAssertions) {};
