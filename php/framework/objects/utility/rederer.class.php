074de40cbed0019870232c8e1c895bca256c0660
Matt Travi <programmer@travi.org>
2011-09-13 18:11:21 -0500

    some refactoring of json renderer to make intent more clear

--- php/framework/objects/utility/rederer.class.php
+++ php/framework/objects/utility/rederer.class.php
@@ -46,13 +46,20 @@ class JsonRenderer extends Renderer
 {
     public function format($data)
     {
-        //TODO: if content is array of size 1, should only encode that element rather than the array
         header('Content-Type: application/json');
 
+        $data = $this->convertObjectsToAssocArrays($data);
+
+        //TODO: if content is array of size 1, should only encode that element rather than the array
+        return json_encode($data);
+    }
+
+    private function convertObjectsToAssocArrays($data)
+    {
         if (is_object($data) || is_array($data)) {
             $data = $this->object_to_array($data);
+            return $data;
         }
-
-        return json_encode($data);
+        return $data;
     }
 }
