d288e1772b675d7c4ecbadfe50605ca270a82737
Matt Travi <programmer@travi.org>
2012-01-16 14:41:36 -0600

    remainder of crud routing tests

--- php/framework/test/php/controller/CrudControllerTest.php
+++ php/framework/test/php/controller/CrudControllerTest.php
@@ -6,36 +6,123 @@ require_once dirname(__FILE__).'/../../../src/http/Request.class.php';
 
 class CrudControllerTest extends PHPUnit_Framework_TestCase
 {
+    const ANY_ID = 42;
     /** @var CrudController */
-    private $controller;
-
+    private $partiallyMockedController;
+    /** @var Response */
     private $response;
+    /** @var Request */
+    private $mockRequest;
+
+    public function setUp()
+    {
+        $this->partiallyMockedController = $this->getMock(
+            'CrudController',
+            array(
+                'getList',
+                'getById',
+                'addToList',
+                'updateById',
+                'deleteById'
+            )
+        );
+
+        $this->response = new Response(array());
+        $this->mockRequest = $this->getMock('Request');
+    }
 
     public function testGetListRoutesToProperMethod()
     {
-        $this->response = new Response(array());
+        $this->mockRequest->expects($this->once())
+            ->method('getRequestMethod')
+            ->will($this->returnValue(Request::GET));
 
-        $mockRequest = $this->getMock('Request');
-        $mockRequest->expects($this->once())
-            ->method('getRequestMethod');
+        $this->mockRequest->expects($this->once())
+            ->method('getId');
 
-        $this->controller = new CrudController();
+        $this->partiallyMockedController->expects($this->once())
+            ->method('getList');
 
-        $this->controller->index($mockRequest, $this->response);
+        $this->partiallyMockedController->index($this->mockRequest, $this->response);
     }
 
     public function testGetByIdRoutesToProperMethod()
     {
-        $this->response = new Response(array());
+        $this->mockRequest->expects($this->once())
+            ->method('getRequestMethod')
+            ->will($this->returnValue(Request::GET));
 
-        $mockRequest = $this->getMock('Request');
-        $mockRequest->expects($this->once())
-            ->method('getRequestMethod');
-        $mockRequest->expects($this->once())
+        $this->mockRequest->expects($this->once())
+            ->method('getId')
+            ->will($this->returnValue(self::ANY_ID));
+
+        $this->partiallyMockedController->expects($this->once())
+            ->method('getById');
+
+        $this->partiallyMockedController->index($this->mockRequest, $this->response);
+    }
+
+    public function testAddTolistRoutesToProperMethod()
+    {
+        $this->mockRequest->expects($this->once())
+            ->method('getRequestMethod')
+            ->will($this->returnValue(Request::POST));
+
+        $this->mockRequest->expects($this->once())
             ->method('getId');
 
-        $this->controller = new CrudController();
+        $this->partiallyMockedController->expects($this->once())
+            ->method('addTolist');
 
-        $this->controller->index($mockRequest, $this->response);
+        $this->partiallyMockedController->index($this->mockRequest, $this->response);
+    }
+
+    public function testUpdateByIdRoutesToProperMethod()
+    {
+        $this->mockRequest->expects($this->once())
+            ->method('getRequestMethod')
+            ->will($this->returnValue(Request::POST));
+
+        $this->mockRequest->expects($this->once())
+            ->method('getId')
+            ->will($this->returnValue(self::ANY_ID));
+
+        $this->partiallyMockedController->expects($this->once())
+            ->method('updateById');
+
+        $this->partiallyMockedController->index($this->mockRequest, $this->response);
+    }
+
+    public function testDeleteListIsNotAllowed()
+    {
+        $this->mockRequest->expects($this->once())
+            ->method('getRequestMethod')
+            ->will($this->returnValue(Request::DELETE));
+
+        $this->mockRequest->expects($this->once())
+            ->method('getId');
+
+        $this->markTestIncomplete('should resolve to a "method not allowed"');
+    }
+
+    public function testDeleteByIdRoutesToProperMethod()
+    {
+        $this->mockRequest->expects($this->once())
+            ->method('getRequestMethod')
+            ->will($this->returnValue(Request::DELETE));
+
+        $this->mockRequest->expects($this->once())
+            ->method('getId')
+            ->will($this->returnValue(self::ANY_ID));
+
+        $this->partiallyMockedController->expects($this->once())
+            ->method('deleteById');
+
+        $this->partiallyMockedController->index($this->mockRequest, $this->response);
+    }
+
+    public function testDefaultImplementationsNotImplemented()
+    {
+        $this->markTestIncomplete('should all resolve to "method not yet implemented"');K
     }
 }
