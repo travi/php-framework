f970ecf9302fc13892dbed7814c56e3bd34f563f
Matt Travi <programmer@travi.org>
2012-02-24 05:55:30 -0600

    checkstyle fix

--- php/framework/test/php/utilities/EnvironmentTest.php
+++ php/framework/test/php/utilities/EnvironmentTest.php
@@ -10,21 +10,24 @@ class EnvironmentTest extends PHPUnit_Framework_TestCase
         $this->environment->setProductionUrl(self::PROD_URL);
     }
 
-    public function testReportedAsLocalWhenContainsLocalTopLevelDomain() {
+    public function testReportedAsLocalWhenContainsLocalTopLevelDomain()
+    {
         $_SERVER['HTTP_HOST'] = 'currentUrl.local';
 
         $this->assertTrue($this->environment->isLocal());
         $this->assertFalse($this->environment->isProduction());
     }
 
-    public function testReportedAsNeitherLocalNorProdWhenNoMatch() {
+    public function testReportedAsNeitherLocalNorProdWhenNoMatch()
+    {
         $_SERVER['HTTP_HOST'] = 'currentUrl';
 
         $this->assertFalse($this->environment->isLocal());
         $this->assertFalse($this->environment->isProduction());
     }
 
-    public function testReportedAsProdWhenContainsProdUrl() {
+    public function testReportedAsProdWhenContainsProdUrl()
+    {
         $_SERVER['HTTP_HOST'] = self::PROD_URL;
 
         $this->assertFalse($this->environment->isLocal());
