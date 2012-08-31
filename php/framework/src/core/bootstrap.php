8d5c754bf5e392e9f23b3c1c0856aa2abbf2ad68
Matt Travi <programmer@travi.org>
2012-08-31 09:35:17 -0500

    turn error output back off after finishing up the refactoring around namespacing

--- php/framework/src/core/bootstrap.php
+++ php/framework/src/core/bootstrap.php
@@ -8,8 +8,8 @@ require_once dirname(__FILE__).'/init.php';
 //error_reporting(E_ALL ^ E_NOTICE);
 // Report all errors except E_NOTICE and E_WARNING (better for production)
 error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
-//ini_set('display_errors', 'Off');
-//ini_set('log_errors', 'On');
+ini_set('display_errors', 'Off');
+ini_set('log_errors', 'On');
 
 /** @var $frontController FrontController */
 $frontController = Pd_Make::name('Travi\\framework\\controller\\front\\FrontController');
