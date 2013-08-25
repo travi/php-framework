1d85ec23942f51f59e7232662481cfc9e9e35f78
Matt Travi <programmer@travi.org>
2013-08-25 14:23:39 -0500

    fix jslint issue

--- client/js/plugins/jquery.formAlign.js
+++ client/js/plugins/jquery.formAlign.js
@@ -5,7 +5,7 @@
     jQuery.fn.alignFields = function () {
         return this.each(function () {
             if (($.browser.mozilla) && (parseFloat($.browser.version) < 1.9)) {
-                $(this).find("ul.fieldList li>label").each(function (i) {
+                $(this).find("ul.fieldList li>label").each(function () {
                     var labelSpan = $('<span>').css('display', 'block')
                         .width($(this).width()).html($(this).html());
                     $(this).empty().append(labelSpan).css('display', '-moz-inline-box');
