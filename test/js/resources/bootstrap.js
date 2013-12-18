ccb9833b084524705dbf6c5f5b8b18644976dad1
Matt Travi <programmer@travi.org>
2013-12-17 18:10:27 -0600

    removed old files that have been extracted as components

--- test/js/resources/bootstrap.js
+++ test/js/resources/bootstrap.js
@@ -1,29 +1,9 @@
 /*global console, jstestdriver*/
-console.log = sinon.log = function () {
+console.log = function () {
     jstestdriver.console.log.apply(jstestdriver.console, arguments);
 };
 
 travi.test = travi.test || {};
 travi.test.testCase = TestCase;
 
-var contextPath;
-if (window.buster) {
-    contextPath = buster.env.contextPath + '/include';
-} else if (jstestdriver) {
-    contextPath = '/test';
-}
-
 $.fx.off = true;
-
-travi.templates.init();
-
-travi.templates.preLoad('chooseEnhancement', contextPath + '/client/templates/enhancementVersion.tmpl');
-
-travi.templates.preLoad('update-item', contextPath + '/test/js/templates/stub-update-item.tmpl');
-travi.templates.preLoad('entityList', contextPath + '/test/js/templates/entity-list.tmpl');
-travi.templates.preLoad('pagination', contextPath + '/test/js/templates/pagination.tmpl');
-
-//$.ajax = function () {
-//    fail('ajax not stubbed properly');
-//};
-
