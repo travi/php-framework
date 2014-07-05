a127dbaf2710c3d70d678de1379419eb30c07b1e
Matt Travi <programmer@travi.org>
2014-07-04 20:06:59 -0500

    replace datepicker init code with a dependency on new js module for #29

--- php/framework/src/travi/framework/components/Forms/inputs/DateInput.php
+++ php/framework/src/travi/framework/components/Forms/inputs/DateInput.php
@@ -2,8 +2,6 @@
 
 namespace travi\framework\components\Forms\inputs;
 
-use travi\framework\components\Forms\inputs\Input;
-
 class DateInput extends Input
 {
     public function __construct($options)
@@ -11,13 +9,6 @@ class DateInput extends Input
         parent::__construct($options);
         $this->setType("date");
         $this->setClass("textInput datepicker");
-        $this->addJavaScript('jqueryUi');
-        $this->addJsInit(
-            "$('input.datepicker').datepicker({
-                dateFormat: 'yy-mm-dd',
-                buttonImage: '/resources/thirdparty/travi-styles/img/calendar.gif',
-                buttonImageOnly: true, showOn: 'both'
-            });"
-        );
+        $this->addJavaScript('datePicker');
     }
 }
--- php/framework/test/php/content/components/form/inputs/DateInputTest.php
+++ php/framework/test/php/content/components/form/inputs/DateInputTest.php
@@ -16,17 +16,6 @@ class DateInputTest extends PHPUnit_Framework_TestCase
     {
         $this->assertSame('date', $this->object->getType());
         $this->assertSame('textInput datepicker', $this->object->getClass());
-        $this->assertSame(array('jqueryUi'), $this->object->getJavaScripts());
-        $this->assertSame(
-            array(
-                 "$('input.datepicker').datepicker({
-                dateFormat: 'yy-mm-dd',
-                buttonImage: '/resources/thirdparty/travi-styles/img/calendar.gif',
-                buttonImageOnly: true, showOn: 'both'
-            });"
-            ),
-            $this->object->getJsInits()
-        );
+        $this->assertSame(array('datePicker'), $this->object->getJavaScripts());
     }
 }
-?>
