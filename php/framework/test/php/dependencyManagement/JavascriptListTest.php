8bede9bf7336733adca5f911335946c3b11fc906
Matt Travi <programmer@travi.org>
2014-11-27 23:06:01 -0600

    cover the empty case

--- php/framework/test/php/dependencyManagement/JavascriptListTest.php
+++ php/framework/test/php/dependencyManagement/JavascriptListTest.php
@@ -7,14 +7,25 @@ use travi\framework\dependencyManagement\JavascriptList;
 
 class JavascriptListTest extends PHPUnit_Framework_TestCase
 {
+    /** @var  JavascriptList */
+    private $list;
+
+    public function setUp()
+    {
+        $this->list = new JavascriptList();
+    }
+
+    public function testThatListIsEmptyIfNothingHasBeenAdded()
+    {
+        $this->assertEquals(array(), $this->list->get());
+    }
 
     public function testThatJavascriptFileCanBeAddedToTheList()
     {
         $someScript = 'jquery';
-        $list = new JavascriptList();
 
-        $list->add($someScript);
+        $this->list->add($someScript);
 
-        $this->assertEquals(array($someScript), $list->get());
+        $this->assertEquals(array($someScript), $this->list->get());
     }
 }
