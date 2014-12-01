351da4ed3669041135da6fe45b7d3e700145cb02
Matt Travi <programmer@travi.org>
2014-11-30 18:56:32 -0600

    made the working-directory change more OS flexible

--- php/framework/test/php/mockProject/install-dependencies.sh
+++ php/framework/test/php/mockProject/install-dependencies.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-cd "$(dirname ${BASH_SOURCE[0]})"
+realpath () {
+  [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+cd $(dirname `readlink -f $0 || realpath $0`)
 
 #bower install --config.interactive=false
 npm install
